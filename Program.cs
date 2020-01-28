1060e67299a9b155cf41ed0049782242fd40c6e2
HongQuan <huynhquandn1999@gmail.com>
2020-01-28 11:18:13 +0700

    Add all file of First game in C#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GameDoanSo
+{
+    class Program
+    {
+        static void Game()
+        {
+            Random rd = new Random();
+            int numberComputer = rd.Next(100);
+            int numberByMe;
+            int numberGuess = 0;
+            Console.WriteLine("Computer Guessed, Please choose the my number");
+            while (true)
+            {
+                
+                numberByMe = int.Parse(Console.ReadLine());
+                numberGuess++;
+                Console.WriteLine("Guess number: {0}", numberGuess);
+                if(numberByMe == numberComputer)
+                {
+                    Console.WriteLine("WIN!!!!!, Computer number is: {0}", numberComputer);
+                    break;
+                }
+                if(numberByMe > numberComputer)
+                {
+                    Console.WriteLine("Your number > Computer number");
+                }
+                else
+                {
+                    Console.WriteLine("Your number < Computer number");
+                }
+                if(numberGuess == 7)
+                {
+                    Console.WriteLine("GAME OVER!!!!");
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Game();
+        }
+    }
+}
